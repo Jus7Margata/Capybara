68552771029cef5f49820de4482a13ed275961cd
kokok <kostovamikaela66@gmail.com>
2023-01-12 12:34:15 +0200

    PiramidaOtChisla

---
+++ NqmaZnachenie/PiramidaOtChisla/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiramidaOtChisla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведете число: ");
+            var n = int.Parse(Console.ReadLine());
+
+
+            int temp = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int a = 0; a < i; a++)
+                {
+                    Console.Write($"{temp++} ");
+                    if (temp == (n+1))
+                        break;
+                }
+                if (temp == (n+1))
+                    break;
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
